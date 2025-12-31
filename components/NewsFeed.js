e9781ad788c34ae1d79f4127c7a2a984a5042e71
JT <jt.turek@microsoft.com>
2025-12-31 00:14:13 -0500

    Switch to relative imports AND fix tsconfig includes

--- components/NewsFeed.js
+++ components/NewsFeed.js
@@ -1,6 +1,6 @@
 import Parser from 'rss-parser';
 
-import { getNews as getPbNews } from '@/lib/pocketbase';
+import { getNews as getPbNews } from '../lib/pocketbase';
 
 async function getNews() {
     // 1. Try fetching from PocketBase CMS
